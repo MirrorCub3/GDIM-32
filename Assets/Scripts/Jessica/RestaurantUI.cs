a888f7e164c8b5642de0c651c54f25015fcdcdb2
Jessica Lam <jhlam5@uci.edu>
2023-01-28 17:36:21 -0800

    Restaurant UI + TMP import
    Restaurant UI showing up with Trigger + imported Text Mesh Pro

---
+++ Assets/Scripts/Jessica/RestaurantUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestaurantUI : MonoBehaviour
+{
+    public GameObject restaurantDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restaurantDisplay.SetActive(false);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            restaurantDisplay.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            restaurantDisplay.SetActive(false);
+            
+        }
+    }
+}
