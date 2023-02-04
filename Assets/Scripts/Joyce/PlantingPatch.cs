76f063e3aca0b296a10f1e5eccbff5f0a124e44e
Joyce Mai <75496558+MirrorCub3@users.noreply.github.com>
2023-02-03 21:10:02 -0800

    Added a locked state to the dirt
    added a locked state to the dirt and a locked icon to display if locked

--- Assets/Scripts/Joyce/PlantingPatch.cs
+++ Assets/Scripts/Joyce/PlantingPatch.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 // Joyce Mai
 public class PlantingPatch : MonoBehaviour
 {
-    private enum DirtStates { WAIT, GROW, SPAWN}
+    public enum DirtStates { LOCKED, GROW, SPAWN, WAIT} // making public such that external programs can set the state of the dirt patch once unlocked
 
     [Header("Card Spawn")]
     [SerializeField] private GameObject spawnPrefab; // will be used to instantiate the card
@@ -14,17 +14,18 @@ public class PlantingPatch : MonoBehaviour
     [SerializeField] private Sweets spawnData;
 
     [Header("Visuals and UI")]
-    [SerializeField] private GameObject sprout;
+    [SerializeField] private Image lockedIcon; // image to display when the patch is not yet usable
+    [SerializeField] private GameObject sprout; // reference to the sprout sprite to display during growth process
     [SerializeField] private Slider plantTimeBar; // ui for the slider showing the growth progress
-    [SerializeField] private Image fill;
-    [SerializeField] private DirtUISettings colors;
+    [SerializeField] private Image fill; // reference to the fill bar image on the progress slider
+    [SerializeField] private DirtUISettings colors; // scriptable object which holds the color data for the color bar changes
 
     [Header("Spawn Data")]
     [SerializeField] private float cooldownMultiplier = 1; // multiplier on the time it takes to cool down
-    private float time;
+    private float time; // used to set the progress bar fill level
 
     [Header("State Variables")]
-    [SerializeField] private DirtStates currState = DirtStates.GROW;
+    [SerializeField] private DirtStates currState = DirtStates.GROW; // used to keep track of the current state
 
     void Start()
     {
@@ -37,9 +38,13 @@ public class PlantingPatch : MonoBehaviour
         {
             Grow();
         }
+        else if (currState == DirtStates.WAIT)
+        {
+            Cooldown();
+        }
         else
         {
-            StartCoroutine(Cooldown());
+            Locked();
         }
 
     }
@@ -75,8 +80,8 @@ public class PlantingPatch : MonoBehaviour
         spawn.GetComponent<RecipeCardCollectable>().ThisCardCollectedNotif += OnCollect;
         plantTimeBar.gameObject.SetActive(false);
     }
-
-    private IEnumerator Cooldown()
+    
+    private void Cooldown()
     {
         currState = DirtStates.WAIT;
         sprout.SetActive(false);
@@ -84,7 +89,11 @@ public class PlantingPatch : MonoBehaviour
         plantTimeBar.value = time;
         plantTimeBar.gameObject.SetActive(true);
         fill.color = colors.waitCol;
+        StartCoroutine(CooldownIE());
+    }
 
+    private IEnumerator CooldownIE()
+    {
         yield return new WaitForSeconds(spawnData.PlantGrowthTime * cooldownMultiplier);
 
         Grow();
@@ -99,12 +108,41 @@ public class PlantingPatch : MonoBehaviour
         fill.color = colors.growCol;
     }
 
-    private void OnCollect()
+    private void Locked() // used when the dirt patch is on the map but yet to become useable
+    {
+        currState = DirtStates.LOCKED;
+        sprout.SetActive(false);
+        plantTimeBar.gameObject.SetActive(false);
+        time = 0;
+        lockedIcon.enabled = true;
+    }
+
+    private void OnCollect() // called via subscription when the spawned card object is collected
     {
         if (currState == DirtStates.WAIT)
             return;
 
         StopAllCoroutines();
-        StartCoroutine(Cooldown());
+        Cooldown();
+    }
+
+    public void Unlock(DirtStates state) // a public method for external code like the store to set the state of dirt once unlocked
+    {
+        currState = state;
+        switch(state)
+        {
+            case DirtStates.GROW:
+                Grow();
+                break;
+            case DirtStates.WAIT:
+                Cooldown();
+                break;
+            case DirtStates.SPAWN:
+                Spawn();
+                break;
+            default:
+                print("You should not be setting to locked state in unlock");
+                break;
+        }
     }
 }
