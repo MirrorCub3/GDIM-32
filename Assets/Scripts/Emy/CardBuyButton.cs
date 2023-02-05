be5114a85d89ccba3d2212a83f7b8d35e410b38c
Joyce Mai <75496558+MirrorCub3@users.noreply.github.com>
2023-02-04 16:20:47 -0800

    started card buy button script

---
+++ Assets/Scripts/Emy/CardBuyButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+// Emy
+public class CardBuyButton : MonoBehaviour
+{
+    [Header("Item Info")]
+    [SerializeField] private Sweets product; // this is a reference to the sweets object that this button purchases
+    [SerializeField] private int cost;
+
+    [Header("Visuals")]
+    [SerializeField] private Image productIcon;
+    [SerializeField] private TextMeshProUGUI costText;
+
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindObjectOfType<Inventory>(); // this is the reference the inventory to update/add to
+
+        productIcon.sprite = product.icon; // change this to reference the UI icon instead
+        // change the costtext to reflect the cost
+    }
+
+    public void Purchase()
+    {
+        inventory.Add(product, 1); // adding one of this sweets object to the inventory
+    }
+}
