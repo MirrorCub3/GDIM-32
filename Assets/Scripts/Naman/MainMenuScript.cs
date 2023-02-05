207b6cff1689d5a6ca7d17f1abd65d8f2a7d1e84
namank100 <khurann1@uci.edu>
2023-02-04 16:41:20 -0800

    ScriptUpdate

--- Assets/Scripts/Naman/MainMenuScript.cs
+++ Assets/Scripts/Naman/MainMenuScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Naman Khurana
 public class MainMenuScript : MonoBehaviour
 {
 
--- Assets/Scripts/Naman/PauseMenu.cs
+++ Assets/Scripts/Naman/PauseMenu.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+//Naman Khurana
+
 public class PauseMenu : MonoBehaviour
 {
 
